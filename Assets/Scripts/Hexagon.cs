f27e8542f7f7f2aa492d690a4e58f0cc3fd8ed6b
Kathryn Bergen <kbergen@chapman.edu>
2025-12-27 13:11:01 -0800

    cleaned up comments

--- Assets/Scripts/Hexagon.cs
+++ Assets/Scripts/Hexagon.cs
@@ -60,50 +60,30 @@ public class Hexagon : MonoBehaviour
         // CURRENT SPRITE SHOULD BE BLANK
         // PREVIOUS END OF CHAIN SHOULD BE UPDATED WITH THE DIRECTION
         HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.Blank];
-        /*
-        if (HexNumber == 0) ; //2
-        else if (HexNumber == 6) ;//2
-        else if (HexNumber == 39) ;//2
-        else if (HexNumber == 45) ;//2
-        else if (HexNumber % 13 == 0) ; //3 (TR, R, BR)
-        else if (HexNumber % 13 == 7) ; //5 (all but left)
-        else if (HexNumber % 13 == 12) ;//5 (all but right)
-        else if (HexNumber % 13 == 6) ; //3 (TL, L, BL)
-        else
-        {*/
-            switch (HexNumber - HexagonMinigameController.GetEndOfChain().HexNumber)
-            {
-                case 7: // bottom right
-                    print("bottom right");
-                    HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.DownRight];
-                    break;
-                case 6: // bottom left
-                    print("bottom left");
-                    HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.DownLeft];
-                    break;
-                case 1: // right
-                    print("right");
-                    HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.Right];
-                    break;
-                case -1: // left
-                    print("left");
-                    HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.Left];
-                    break;
-                case -6: // top right
-                    print("top right");
-                    HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.UpRight];
-                    break;
-                case -7: // top left
-                    HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.UpLeft];
-                    print("top left");
-                    break;
-                default:
-                    print("uh oh- "+(HexNumber - HexagonMinigameController.GetEndOfChain().HexNumber));
-                    break;
-            }
-        //}
-        
-
+        switch (HexNumber - HexagonMinigameController.GetEndOfChain().HexNumber)
+        {
+            case 7: // bottom right
+                HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.DownRight];
+                break;
+            case 6: // bottom left
+                HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.DownLeft];
+                break;
+            case 1: // right
+                HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.Right];
+                break;
+            case -1: // left
+                HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.Left];
+                break;
+            case -6: // top right
+                HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.UpRight];
+                break;
+            case -7: // top left
+                HexagonMinigameController.GetEndOfChain().HexagonImage.sprite = HexagonSelectedDirectionalSprites[(int)HexagonDirectionType.UpLeft];
+                break;
+            default:
+                print("something went wrong-offset= "+(HexNumber - HexagonMinigameController.GetEndOfChain().HexNumber));
+                break;
+        }
     }
 
     public void TryToDeselect(Hexagon hex)
